3cd3494fb8f68135aa7039de7995475f34feb533
Richard <rahayn@yahoo.com>
2017-09-27 13:38:10 -0400

    find percents
    Added more output

--- FindPercents/Program.cs
+++ FindPercents/Program.cs
@@ -6,21 +6,21 @@ namespace FindPercents
     {
         public static void Main(string[] args)
         {
-            double startingBaseNum = 0;
-            double removedNum = 0;
+            double startingBaseNum,removedNum, percentageOutcome, percentageOf, percentageResult;
+
 
             Console.WriteLine("Hi! Please enter the starting amount number");
             startingBaseNum = double.Parse(Console.ReadLine());
 			Console.WriteLine("Awesome now enter the number being removed amount number");
 			removedNum = double.Parse(Console.ReadLine());
-            double percentageOutcome = removedNum / startingBaseNum * 100;
+            percentageOutcome = removedNum / startingBaseNum * 100;
 
             Console.WriteLine("Ok tha number makes uo {0}% of{1}",percentageOutcome,startingBaseNum );
 
             Console.WriteLine("Now to find the value of a percentages! We can kee using the same base so enter in a percentage. Leave off the % sign");
-            double percentageOf = double.Parse(Console.ReadLine());
-            double percentageResult = percentageOf * 0.01 * startingBaseNum;
-            Console.WriteLine(percentageResult);
+            percentageOf = double.Parse(Console.ReadLine());
+            percentageResult = percentageOf * 0.01 * startingBaseNum;
+            Console.WriteLine("Ok {0} is {1}% of {2} ",percentageResult, percentageOf, startingBaseNum);
         }
     }
 }
